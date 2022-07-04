d76bf6baf02963e8279805c4c5613bc8aa03854a
Ps1xe <evgesha228997@gmail.com>
2022-07-04 17:31:47 +0600

    add master branch

--- src/utils/consts.tsx
+++
@@ -1,8 +0,0 @@
-export enum ConstsRoutes {
-    MAIN_ROUTE = '/main',
-    CONTACS_ROUTE = '/contacs',
-    ABOUT_ROUTE = '/about',
-    HOME_ROUTE = '/home',
-    LOGIN_ROUTE = '/login',
-    REGISTRATION_ROUTE = '/reg',
-}
