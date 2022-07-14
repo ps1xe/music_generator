e0222cbfeb22a3dfc6dd866abebbdc4e2ddecb31
Ps1xe <evgesha228997@gmail.com>
2022-07-14 22:35:43 +0600

    add Procfile, deploy heroku

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const PORT = process.env.PORT;
 
   const config = new DocumentBuilder()
     .setTitle('Music generator')
@@ -13,6 +14,6 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(3000);
+  await app.listen(PORT || 3000);
 }
 bootstrap();
